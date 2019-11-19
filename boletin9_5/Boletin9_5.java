bf38ab74d42dd26dbd5302ae2061bc896e7e38df
Orlando Joel Yagual Endara <DANIELCASTELAO\oyagualendara@nautilus31.ad.danielcastelao.org>
2019-11-19 11:08:24 +0100

    first commit

---
+++ boletin9_5/Boletin9_5.java
@@ -0,0 +1,15 @@
+
+package boletin9_5;
+
+public class Boletin9_5 {
+
+
+    public static void main(String[] args) {
+          Elementos obx = new Elementos();
+        obx.Elemento();
+        obx.Elemento2();
+obx.Elemento3();
+    }
+}
+
+
